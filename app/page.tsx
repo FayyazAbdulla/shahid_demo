d8906a357b1da560ec06a8f8ffc4e71a812b7331
Abdul <abdx2228@gmail.com>
2026-03-03 00:57:42 +0530

    feat(ui): add lamp demo component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Component from '@/components/3d-globe-demo-3'
 import HeroScrollDemo from '@/components/ui/container-scroll-animation-demo'
 import DraggableCardDemo from '@/components/ui/draggable-card-demo-2'
 import KeyboardDemo from '@/components/ui/keyboard-demo'
+import LampDemo from '@/components/ui/lamp-demo'
 import SparklesPreview from '@/components/ui/sparkles-demo'
 import SparklesFullpagePreview from '@/components/ui/sparkles-fullpage-demo'
 import TextHoverEffectDemo from '@/components/ui/text-hover-effect-demo'
@@ -14,6 +15,7 @@ export default function Page() {
       <HeroScrollDemo />
       <DraggableCardDemo />
       <KeyboardDemo />
+      <LampDemo />
       <SparklesPreview />
       <SparklesFullpagePreview />
       <TextHoverEffectDemo />
