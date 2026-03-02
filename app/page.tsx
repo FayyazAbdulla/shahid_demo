ee9cd157f171104091364abfeed34448915fde48
Abdul <abdx2228@gmail.com>
2026-03-03 00:56:24 +0530

    feat(ui): add container scroll animation demo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Component from '@/components/3d-globe-demo-3'
+import HeroScrollDemo from '@/components/ui/container-scroll-animation-demo'
 import DraggableCardDemo from '@/components/ui/draggable-card-demo-2'
 import KeyboardDemo from '@/components/ui/keyboard-demo'
 import SparklesPreview from '@/components/ui/sparkles-demo'
@@ -10,6 +11,7 @@ export default function Page() {
   return (
     <main className="w-full">
       <Component />
+      <HeroScrollDemo />
       <DraggableCardDemo />
       <KeyboardDemo />
       <SparklesPreview />
